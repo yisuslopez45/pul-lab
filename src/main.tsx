1408173ea177475aea4c60c6f35de499b75fb525
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-02-25 01:04:31 -0500

    feat: :sparkles: se configura el main con las rutas y el layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './layout/Layout'
+// PAGES
+import App from './App'
+import Page1 from './pages/page-1/Page1'
+import Page2 from './pages/page-2/Page2'
+import NotFound from './pages/not-found/NotFound'
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/page-1" element={<Page1 />} />
+      <Route path="/page-2" element={<Page2 />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Layout>
+</BrowserRouter>
 )
