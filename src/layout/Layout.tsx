431595585aa47a3dcc7487cd0db17fc94b25187e
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-02-25 01:02:30 -0500

    feat: :sparkles: se crea el layout inicial haciendo uso de tailwind CSS

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { JSX } from 'react'
+import Header from './header/Header'
+import Footer from './footer/Footer'
+
+const Layout = ( {  children } : { children : JSX.Element } ) => {
+    return (
+      <div className="flex flex-col min-h-screen" >
+          <Header/>
+           <main className="flex-grow" >{children}</main>
+          <Footer/>
+      </div>
+    )
+  }
+  
+  export default Layout
