feb755b72d30ff566b104a53f52efd5288da596c
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-02-27 23:23:03 -0500

    fix: :bug: se corrige error en la ruta del logo

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/public/logo.svg" alt="Flowbite Logo" className="h-10 w-auto" />
+            <img src="/Logo.svg" alt="Flowbite Logo" className="h-10 w-auto" />
             <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">PulLab</span>
           </a>
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
