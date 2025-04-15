336a62c1de7b7f04b9289de81534c4c864246eba
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-04-14 23:50:28 -0500

    feat: :sparkles: se realiza ajustes visuales sobre el headers

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,45 +1,95 @@
+import { useState } from "react"
 import { NavLink } from "react-router"
 
+{/* <NavLink
+className="block py-2 px-3 text-white-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+to="/page-1"
+end
+>
+Ruta 1
+</NavLink> */}
+
+
+{/* <NavLink
+className="block py-2 px-3 text-white-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+to="/page-1"
+end
+>
+Ruta 1
+</NavLink> */}
+
 const Header = () => {
+
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null)
+
+  const toggleDropdown = (name: string) => {
+    if (openDropdown === name) {
+      setOpenDropdown(null)
+    } else {
+      setOpenDropdown(name)
+    }
+  }
+
   return (
     <header  >
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/Logo.svg" alt="Flowbite Logo" className="h-10 w-auto" />
-            <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">PulLab</span>
-          </a>
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-          </button>
-          <div
-            className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul
-              className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <NavLink
-                  className="block py-2 px-3 text-white-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  to="/page-1"
-                  end
-                >
-                  Ruta 1
-                </NavLink>
-              </li>
+      <nav className="bg-lila border-gray-200 px-20 max-screen-xl flex items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/Logo.svg" alt="Flowbite Logo" className="h-10 w-auto" />
+          <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">PulLab</span>
+        </a>
 
-              <li>
-                <NavLink
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  to="/page-2"
-                  end
-                >
-                  Ruta 2
-                </NavLink>
-              </li>
-            </ul>
+        <div className="flex gap-3">
+
+          <ul className="flex gap-3" >
+            <li className="relative" >
+              <a onClick={()=>toggleDropdown("enfermedad")}  className="bg-white p-2 rounded-3xl font-bold flex text-gray-800" >
+                ENFERMEDAD
+                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
+                </svg>
+              </a>
+              {openDropdown === "enfermedad" && (
+                <div className="absolute left-0 mt-2 w-100 rounded-md             shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10">
+                   <div className="py-1" role="menu"               aria-orientation="vertical">
+                      <h1>HOLA</h1>
+                   </div>
+                </div>
+              )}
+            </li>
+
+            <li>
+              <a className="bg-white p-2 rounded-3xl font-bold flex text-gray-800" >
+                QUIZ
+                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
+                </svg>
+              </a>
+            </li>
+
+            <li>
+              <a className="bg-white p-2 rounded-3xl font-bold flex text-gray-800" >
+                ENFERMEDAD
+                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
+                </svg>
+              </a>
+            </li>
+
+          </ul>
+
+          <div className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+            <svg className="w-7 h-7 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m13 19 3.5-9 3.5 9m-6.125-2h5.25M3 7h7m0 0h2m-2 0c0 1.63-.793 3.926-2.239 5.655M7.5 6.818V5m.261 7.655C6.79 13.82 5.521 14.725 4 15m3.761-2.345L5 10m2.761 2.655L10.2 15" />
+            </svg>
           </div>
+
+          <div className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+            <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clip-rule="evenodd" />
+            </svg>
+
+          </div>
+
         </div>
       </nav>
 
