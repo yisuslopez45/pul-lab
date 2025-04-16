bccb5d56b5ee2be146b5b108561cc50ac67da4ed
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-04-16 00:48:39 -0500

    feat: :sparkles: se agrega el color del bg

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,9 +4,9 @@ import Footer from './footer/Footer'
 
 const Layout = ( {  children } : { children : JSX.Element } ) => {
     return (
-      <div className="flex flex-col min-h-screen" >
+      <div className="flex flex-col min-h-screen bg-linear-to-bl from-lila-600 to-lila-100" >
           <Header/>
-           <main className='flex-grow flex  ' >{children}</main>
+           <main className='flex-grow flex ' >{children}</main>
           <Footer/>
       </div>
     )
