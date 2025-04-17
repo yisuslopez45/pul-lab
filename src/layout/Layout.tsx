e09f050de6bdb3ab01ae1b66ceb941734fafbe45
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-04-17 02:24:28 -0500

    feat: :sparkles: se confgura el color y el width del visot

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,9 +4,9 @@ import Footer from './footer/Footer'
 
 const Layout = ( {  children } : { children : JSX.Element } ) => {
     return (
-      <div className="flex flex-col min-h-screen bg-linear-to-bl from-lila-600 to-lila-100" >
+      <div className="flex flex-col min-h-screen bg-linear-to-bl from-lila-600 to-lila-100 " >
           <Header/>
-           <main className='flex-grow flex ' >{children}</main>
+           <main className='flex-grow sm:max-w-full md:max-w-full lg:max-w-7xl mx-auto' >{children}</main>
           <Footer/>
       </div>
     )
