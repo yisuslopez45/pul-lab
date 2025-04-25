b7839fe1fefb8f166f5dfb4428b50e6f35553c97
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-04-25 02:50:09 -0500

    refactor: :recycle: se refactoriza el layout  en donde se renderizan todos los componentes

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,7 +6,9 @@ const Layout = ( {  children } : { children : JSX.Element } ) => {
     return (
       <div className="flex flex-col min-h-screen bg-linear-to-bl from-lila-600 to-lila-100 " >
           <Header/>
-           <main className='flex-grow sm:max-w-full md:max-w-full lg:max-w-7xl mx-auto' >{children}</main>
+          <section className='flex-grow flex justify-center' >
+           <main className=' sm:max-w-full md:max-w-full lg:max-w-7xl w-full ' >{children}</main>            
+          </section>
           <Footer/>
       </div>
     )
