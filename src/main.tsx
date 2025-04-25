224fbca88f9caecf528c50afdd5dd4eb4c0f49aa
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-04-25 02:49:12 -0500

    feat: :sparkles: se importan las rutas nuevas y las rutas hijas de la enfermedad de neumonia

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Layout from './layout/Layout'
 // PAGES
 import App from './App'
-import Page1 from './pages/page-1/Page1'
+import Pneumonia from './pages/disease-pneumonia/Pneumonia'
 import Page2 from './pages/page-2/Page2'
 import NotFound from './pages/not-found/NotFound'
+
+// Pneumonia
+import WhatIsPneumonia from './pages/disease-pneumonia/components/WhatIs'
+import PreventionPneumonia from './pages/disease-pneumonia/components/Prevention'
+import SymptomsPneumonia from './pages/disease-pneumonia/components/Symptoms'
+import TreatmentPneumonia from './pages/disease-pneumonia/components/Treatment'
 
 
 createRoot(document.getElementById('root')!).render(
@@ -14,7 +20,14 @@ createRoot(document.getElementById('root')!).render(
   <Layout>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/page-1" element={<Page1 />} />
+      
+      <Route path="/enfermedad-neumonia" element={<Pneumonia />}>
+        <Route path="que-es" element={<WhatIsPneumonia />} />
+        <Route path="prevencion" element={<PreventionPneumonia />} />
+        <Route path="sintomas" element={<SymptomsPneumonia />} />
+        <Route path="tratamiento" element={<TreatmentPneumonia />} />
+      </Route>
+      
       <Route path="/page-2" element={<Page2 />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
