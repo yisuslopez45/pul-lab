6cd4a6e2229d5d40ff6a5d9aa04549f2a0b33b25
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-06-13 00:19:04 -0500

    feat: :sparkles: se corrige las state del store

--- src/pages/quiz/store/useStoreQuiz.ts
+++ src/pages/quiz/store/useStoreQuiz.ts
@@ -1,57 +1,63 @@
-import { create } from 'zustand';
-
+import { create } from "zustand";
 
 interface Item {
-    question: string,
-    correct: string,
-    selected: string,
+  question: string;
+  correct: string;
+  selected: string;
 }
 
 interface State {
-    indexQuestion : number | null;
-    questions: Item[];
-    setStateResponse: (value: string, index: number) => void;
-    setIndexQuestion: (index: number) => void;
-    getQuestion : (index : number)=> Item
-
+  indexQuestion: number | null;
+  questions: Item[];
+  setStateResponse: (value: string, index: number) => void;
+  setIndexQuestion: (index: number) => void;
+  getQuestion: (index: number) => Item;
+  resetStore: () => void;
 }
 
+const initialQuestions = [
+  {
+    question: "Seleccione la traquea",
+    correct: "Trachea",
+    selected: "",
+  },
+  {
+    question: "Seleccione el pulmon",
+    correct: "Lung",
+    selected: "",
+  },
+  {
+    question: "Seleccione el diaframa",
+    correct: "Diaphragm",
+    selected: "",
+  },
+];
+
 const useStoreQuiz = create<State>((set, get) => ({
-    indexQuestion : null,
-    questions: [
-        {
-            question: "Seleccione la traquea",
-            correct: "Trachea",
-            selected: '',
-        },
-        {
-            question: "Seleccione el pulmon",
-            correct: "Lung",
-            selected: '',
-        },
-        {
-            question: "Seleccione el diaframa",
-            correct: "Diaphragm",
-            selected: '',
-        },
-    ],
-    
-    getQuestion: (index: number)=> {
-        const items = [...get().questions];
-       return items[index]
-    },
+  indexQuestion: null,
+  questions: [...initialQuestions],
 
-    setStateResponse: (value: string, index: number) => {
-        const updated = [...get().questions];
-        updated[index] = { ...updated[index], selected: value };
-        set({ questions: updated });
-    },
+  getQuestion: (index: number) => {
+    const items = [...get().questions];
+    return items[index];
+  },
 
-    setIndexQuestion(index : number) {
-       set({ indexQuestion : index}) 
-    },
+  setStateResponse: (value: string, index: number) => {
+    const updated = [...get().questions];
+    updated[index] = { ...updated[index], selected: value };
+    set({ questions: updated });
+  },
 
+  setIndexQuestion(index: number) {
+    set({ indexQuestion: index });
+  },
 
-}))
+  resetStore() {
+    set({
+      indexQuestion: null,
+      questions: [...initialQuestions],
+    });
+  },
+}));
 
 export default useStoreQuiz;
