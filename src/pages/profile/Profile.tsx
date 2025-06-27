c18023cc621edf369666f5f3aaecfb09e1a96144
yisuslopez45 <chuchodavidlopez@gmail.com>
2025-06-27 00:27:40 -0500

    feat: :sparkles: se implemento la consulta para ver la informacion del usuario en sesion

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,28 +1,61 @@
-import { CircleUserRound, Heart, Mail, Shield, Star, Sword, Zap } from "lucide-react";
+import {
+  CircleUserRound,
+  Heart,
+  Mail,
+  Shield,
+  Star,
+  Sword,
+  Zap,
+} from "lucide-react";
 import StatBar from "./components/StatBar";
 import { useAuthStore } from "../../store/authStore";
 import CardIU from "../../layout/components/CardIU";
+import { useEffect, useState } from "react";
+import { getAttemptsByCurrentUser, Quiz } from "../quiz/services/service_db";
 
 const Profile = () => {
+  const { userLooged } = useAuthStore();
+  const [attempts, setAttempts] = useState<Quiz[]>([]);
 
-  const { userLooged } = useAuthStore()
+  useEffect(() => {
+    const fetchAttempts = async () => {
+      if (userLooged) {
+        const res = await getAttemptsByCurrentUser(userLooged);
+        setAttempts(res);
+      }
+    };
+
+    fetchAttempts();
+  }, [userLooged]);
+
+  const totalAciertos = attempts.reduce((acc, curr) => {
+    const respuestas = Object.values(curr.answers);
+    const aciertos = respuestas.filter((v) => v === true).length;
+    return acc + aciertos;
+  }, 0);
+
+  const totalErrores = attempts.reduce((acc, curr) => {
+    const respuestas = Object.values(curr.answers);
+    const aciertos = respuestas.filter((v) => v === false).length;
+    return acc + aciertos;
+  }, 0);
+
+
 
   return (
     <>
       <div className=" bg-indigo-900/80 text-white  p-4 flex  justify-center items-center mt-5 rounded-lg py-10">
-
         <div className="grid grid-cols-1 md:grid-cols-6 gap-4 ">
           <div className="md:col-span-2">
-            <CardIU isShowClose={false} title="Perfil"  >
+            <CardIU isShowClose={false} title="Perfil">
               <div className="flex flex-col items-center">
                 <div className="w-24 h-24 border-4 border-lila-200 rounded-lg mb-3 overflow-hidden relative">
                   <img
-                    src={`${ userLooged?.photoURL}`}
+                    src={`${userLooged?.photoURL}`}
                     alt="Logo de React"
                     width="200"
                     className="w-full h-full object-cover"
                   />
-
                 </div>
                 <div className="flex items-center gap-1 mb-3 px-2 py-1 bg-white  text-indigo-900 rounded-4xl">
                   <CircleUserRound />
@@ -42,28 +75,28 @@ const Profile = () => {
           </div>
 
           <div className="md:col-span-4">
-            <CardIU isShowClose={false} title="Caracteristicas" >
+            <CardIU isShowClose={false} title="Caracteristicas">
               <div className="space-y-2">
                 <div className="space-y-3">
                   <StatBar
+                    label="INTENTOS"
+                    value={attempts.length}
+                    icon={<Heart className="w-5 h-5" />}
+                    color="green"
+                  />
+                  <StatBar
                     label="ACIERTOS"
-                    value={1}
+                    value={totalAciertos}
                     icon={<Sword className="w-5 h-5" />}
                     color="red"
                   />
                   <StatBar
                     label="ERRORES"
-                    value={7}
+                    value={totalErrores}
                     icon={<Shield className="w-5 h-5" />}
                     color="blue"
                   />
-                  <StatBar
-                    label="INTENTOS"
-                    value={1222}
-                    icon={<Heart className="w-5 h-5" />}
-                    color="green"
-                  />
-                  <StatBar
+                  {/* <StatBar
                     label="FALLAS"
                     value={12}
                     icon={<Star className="w-5 h-5" />}
@@ -74,14 +107,16 @@ const Profile = () => {
                     value={12}
                     icon={<Zap className="w-5 h-5" />}
                     color="yellow"
-                  />
+                  /> */}
                 </div>
 
                 <div className="mt-6 p-3 border-2 border-dashed border-lila-600 bg-indigo-950 rounded-lg">
                   <h4 className="text-sm font-bold text-white mb-2">
                     PUNTUACION
                   </h4>
-                  <p className="text-sm text-gray-300">+5 ACIERTOS, +3 ERRORES</p>
+                  <p className="text-sm text-gray-300">
+                    +{totalAciertos } ACIERTOS, +{totalErrores} ERRORES
+                  </p>
                 </div>
               </div>
             </CardIU>
