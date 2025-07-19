62206d40bf4204d2b63ff849b899d8daa0d060c0
gustavoa6791 <gustavo.arango6791@hotmail.com>
2025-07-18 20:47:46 -0500

    test

--- src/pages/disease-asthma/canvas/InhalerAsthmaCanva.tsx
+++ src/pages/disease-asthma/canvas/InhalerAsthmaCanva.tsx
@@ -1,4 +1,4 @@
-import { Suspense, /* useEffect, */ useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { OrbitControls, Environment, Sparkles, Text3D, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Vector3 } from 'three';
@@ -8,26 +8,26 @@ import FloorModel from "../models-3d/FloorModel";
 const InhalerAsthmaCanva = () => {
   const [controlsEnabled, setControlsEnabled] = useState(false);
   const [modelRotationY, setModelRotationY] = useState(45);
-  //const [triggerAnimation, setTriggerAnimation] = useState(false);
+  const [triggerAnimation, setTriggerAnimation] = useState(false);
   const position: Vector3 = new Vector3(0, -30, 0);
   const positionText: Vector3 = new Vector3(-40, 18, 0);
 
-  /* const handleKeyDown = (event: KeyboardEvent) => {
+  const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key.toLowerCase() === "x") {
       setTriggerAnimation(true);
       setTimeout(() => setTriggerAnimation(false), 100);
     }
-  }; */
+  };
 
   const handleRotate = (direction: "left" | "right") => {
     const delta = direction === "left" ? -0.2 : 0.2;
     setModelRotationY(prev => prev + delta);
   };
 
-  /* useEffect(() => {
+  useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []); */
+  }, []);
 
   return (
     <div style={{ position: "relative", width: "100%", height: "70vh" }}>
@@ -87,7 +87,7 @@ const InhalerAsthmaCanva = () => {
               scale={9}
               position={[0, -15, 0]}
               castShadow
-              triggerAnimation={false}
+              triggerAnimation={triggerAnimation}
             />
             <Sparkles size={40} scale={95} speed={10} />
           </group>
